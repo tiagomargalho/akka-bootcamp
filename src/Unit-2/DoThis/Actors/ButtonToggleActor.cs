e69be25f5a783b5b63772cff7605bb3211c0c8a5
Tiago Margalho <tiagomargalho@gmail.com>
2016-05-30 17:07:45 +0100

    Lesson 2.3 done.

---
+++ src/Unit-2/DoThis/Actors/ButtonToggleActor.cs
@@ -0,0 +1,66 @@
+﻿using Akka.Actor;
+using System.Windows.Forms;
+using static ChartApp.Actors.ChartingMessages;
+
+namespace ChartApp.Actors
+{
+    class ButtonToggleActor : UntypedActor
+    {
+        /// <summary>
+        /// Toggles this button on or off and sends an appropriate messages
+        /// to the <see cref="PerformanceCounterCoordinatorActor"/>
+        /// </summary>
+        public class Toggle { }
+
+        private readonly CounterType _myCounterType;
+        private bool _isToggledOn;
+        private readonly Button _myButton;
+        private readonly IActorRef _coordinatorActor;
+
+        public ButtonToggleActor(IActorRef coordinatorActor, Button myButton,
+                CounterType myCounterType, bool isToggledOn = false)
+        {
+            _coordinatorActor = coordinatorActor;
+            _myButton = myButton;
+            _isToggledOn = isToggledOn;
+            _myCounterType = myCounterType;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Toggle && _isToggledOn)
+            {
+                // toggle is currently on
+
+                // stop watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Unwatch(_myCounterType));
+
+                FlipToggle();
+            }
+            else if (message is Toggle && !_isToggledOn)
+            {
+                // toggle is currently off
+
+                // start watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Watch(_myCounterType));
+
+                FlipToggle();
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+
+        private void FlipToggle()
+        {
+            // flip the toggle
+            _isToggledOn = !_isToggledOn;
+
+            // change the text of the button
+            _myButton.Text = string.Format("{0} ({1})",
+                _myCounterType.ToString().ToUpperInvariant(),
+                _isToggledOn ? "ON" : "OFF");
+        }
+    }
+}
--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
+using static ChartApp.Actors.ChartingMessages;
 
 namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
-        #region Messages
+        /// <summary>
+        /// Maximum number of points we will allow in a series
+        /// </summary>
+        public const int MaxPoints = 250;
+
+        /// <summary>
+        /// Incrementing counter we use to plot along the X-axis
+        /// </summary>
+        private int xPosCounter = 0;
 
         public class InitializeChart
         {
@@ -33,7 +42,18 @@ namespace ChartApp.Actors
             public Series Series { get; private set; }
         }
 
-        #endregion
+        /// <summary>
+        /// Remove an existing <see cref="Series"/> from the chart
+        /// </summary>
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
 
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
@@ -49,43 +69,94 @@ namespace ChartApp.Actors
 
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
         }
-
-        #region Individual Message Type Handlers
 
         private void HandleInitialize(InitializeChart ic)
         {
             if (ic.InitialSeries != null)
             {
-                //swap the two series out
+                // swap the two series out
                 _seriesIndex = ic.InitialSeries;
             }
 
-            //delete any existing series
+            // delete any existing series
             _chart.Series.Clear();
 
-            //attempt to render the initial chart
+            // set the axes up
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
+            // attempt to render the initial chart
             if (_seriesIndex.Any())
             {
                 foreach (var series in _seriesIndex)
                 {
-                    //force both the chart and the internal index to use the same names
+                    // force both the chart and the internal index to use the same names
                     series.Value.Name = series.Key;
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
         }
 
         private void HandleAddSeries(AddSeries series)
         {
             if (!string.IsNullOrEmpty(series.Series.Name) &&
-            !_seriesIndex.ContainsKey(series.Series.Name))
+                !_seriesIndex.ContainsKey(series.Series.Name))
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
             }
         }
 
-        #endregion
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) &&
+                _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) &&
+                _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
+
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
     }
 }
